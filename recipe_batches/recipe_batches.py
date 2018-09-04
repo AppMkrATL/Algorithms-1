c5d03b26fa975a856a937bb03cb2451b6c7218b6
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-09-04 17:24:46 -0600

    Finished recipe_batches.py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,18 +3,29 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  result = 100
-  for rkey,rval in recipe.items():
-    for ikey,ival in ingredients.items():
-      if rval/ival <= 1:
-        # We have enough
-        if math.ceil(ival/rval) < result:
-          result = math.ceil(ival/rval)
-        pass
-      elif rval/ival > 1:
-        # We dont have enough
-        result = 0
-  return result
+  # result = 100
+  # for rkey,rval in recipe.items():
+  #   for ikey,ival in ingredients.items():
+  #     if rval/ival <= 1:
+  #       # We have enough
+  #       if math.floor(ival/rval) < result:
+  #         result = math.floor(ival/rval)
+  #       pass
+  #     elif rval/ival > 1:
+  #       # We dont have enough
+  #       result = 0
+  # return result
+  min_ratio = math.inf
+  for ingredient, amount in recipe.items():
+    if ingredient not in ingredients:
+      return 0
+    ratio = ingredients[ingredient] // amount
+    if ratio == 0:
+      return 0
+    if ratio < min_ratio:
+      min_ratio = ratio
+
+  return min_ratio
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
